b1d21cbd3b8cdfded0b6d057710ae43750032059
The Sun <156376854+thecaprisun@users.noreply.github.com>
2024-02-27 19:43:26 +0100

    Delete server-sshfs.sh

--- server-sshfs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo sshfs -o rw,allow_other,default_permissions thesun@192.168.1.170:/ /mnt/server
-sudo sshfs -o rw,allow_other,default_permissions thesun@192.168.1.170:/mnt/storage /mnt/server-storage
